b2f886e8aee23a3bf1c04f6c4401031a4a245d85
tinghaolai <analth355@gmail.com>
2022-04-29 08:39:32 +0800

    Insert current time displaying to foreground

--- assets/js/components/Popup.vue
+++ assets/js/components/Popup.vue
@@ -2,6 +2,7 @@
     <div>
         <el-form>
             <el-form-item :label="'Second: ' + currentSecond">
+                <el-button type="Success" @click="currentTabShowSecond">To current tab</el-button>
             </el-form-item>
             <el-form-item label="ATRs">
                 <el-button type="primary" @click="ATRs.push(ATRData())">Add</el-button>
@@ -51,7 +52,69 @@
                 setInterval(() => {
                     this.currentSecond = moment().format("ss");
                 }, 1000);
-            }
+            },
+            currentTabShowSecond() {
+                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                    chrome.scripting.executeScript({
+                        target: { tabId: tabs[0].id },
+                        func: () => {
+                            let display = document.createElement('div');
+                            display.width = '50px';
+                            display.height = '50px';
+                            display.id = 'chrome-extension-trading-tool-foreground-display';
+                            display.style.zIndex  = 999999999999999;
+
+                            setInterval(() => {
+                                display.innerText = 'current second: ' + (new Date()).getSeconds();
+                            }, 1000);
+
+                            draggable(display, 10, 0);
+                            function draggable(element, topPercentage, leftPercentage) {
+                                let originTop = window.innerHeight * topPercentage / 100;
+                                let originLeft = window.innerHeight * leftPercentage / 100;
+                                let isMouseDown = false;
+
+                                element.style.position = 'fixed';
+                                element.style.top = originTop + 'px';
+                                element.style.left = originLeft + 'px';
+
+                                var mouseX;
+                                var mouseY;
+                                var elementX = originLeft;
+                                var elementY = originTop;
+
+                                element.addEventListener('mousedown', onMouseDown);
+                                function onMouseDown(event) {
+                                    mouseX = event.clientX;
+                                    mouseY = event.clientY;
+                                    isMouseDown = true;
+                                }
+
+                                element.addEventListener('mouseup', onMouseUp);
+
+                                function onMouseUp(event) {
+                                    isMouseDown = false;
+                                    elementX = parseInt(element.style.left) || 0;
+                                    elementY = parseInt(element.style.top) || 0;
+                                }
+
+                                document.addEventListener('mousemove', onMouseMove);
+
+                                function onMouseMove(event) {
+                                    if (!isMouseDown) return;
+                                    var deltaX = event.clientX - mouseX;
+                                    var deltaY = event.clientY - mouseY;
+                                    element.style.left = elementX + deltaX + 'px';
+                                    element.style.top = elementY + deltaY + 'px';
+                                }
+                            }
+
+                            document.querySelector('body').appendChild(display);
+                        },
+                    }, (results) => {});
+                })
+
+            },
         },
         created() {
             this.syncSecond();
